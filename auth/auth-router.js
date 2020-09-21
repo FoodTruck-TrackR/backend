ec8bd572d8c8feae8e60b063087c1649355267cc
Nestor <ncervan87@gmail.com>
2020-09-21 12:14:42 -0700

    create knex schema

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -6,6 +6,10 @@ router.get('/register', (req, res) => {
     res.status(200).json({ message: 'end point for auth' })
 })
 
+router.get('/login', (req, res) => {
+    res.status(200).json({ message: 'end point for login' })
+})
+
 
 
 module.exports = router
---
+++ data/migrations/20200921105945_create-user.js
@@ -0,0 +1,55 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments('id')
+        tbl.string('username').notNull().unique()
+        tbl.string('email').notNull().unique()
+        tbl.string('password').notNull()
+        tbl.string('role').notNull()
+    })
+        .createTable('vendors', tbl => {
+            tbl.increments()
+            tbl.string('username').notNull().unique()
+            tbl.string('email').notNull().unique()
+            tbl.string('password').notNull()
+            tbl.string('role').notNull()
+        })
+        .createTable('trucks', tbl => {
+            tbl.increments('id'),
+                tbl.string('name').notNull()
+            tbl.string('location').notNull()
+            tbl.integer('vendor_id').unsigned().notNull().references('id').inTable('vendors').onUpdate('CASCADE').onDelete('CASCADE')
+        })
+        .createTable('items', tbl => {
+            tbl.increments()
+            tbl.string('name').notNull()
+            tbl.string('description').notNull()
+            tbl.string('photo_url').notNull().unique()
+            tbl.float('price').notNull()
+            tbl.float('average_rating')
+            tbl.integer('truck_id').unsigned().notNull().references('id').inTable('vendors').onUpdate('CASCADE').onDelete('CASCADE')
+        })
+        //table will hold user ratings. 
+        .createTable('users_items', tbl => {
+            tbl.increments()
+            tbl.integer('user_id').unsigned().notNull().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
+            tbl.integer('item_id').unsigned().notNull().references('id').inTable('items').onUpdate('CASCADE').onDelete('CASCADE')
+            tbl.integer('ratings').unsigned().notNull()
+        })
+        .createTable('users_trucks', tbl => {
+            tbl.increments()
+            tbl.integer('user_id').unsigned().notNull().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
+            tbl.integer('truck_id').unsigned().notNull().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
+        })
+};
+
+exports.down = function (knex) {
+
+    return knex.schema
+        .dropTableIfExists('users_trucks')
+        .dropTableIfExists('users_items')
+        .dropTableIfExists('items')
+        .dropTableIfExists('trucks')
+        .dropTableIfExists('vendors')
+        .dropTableIfExists('users')
+};
