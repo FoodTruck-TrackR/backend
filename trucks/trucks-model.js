d2a34e8c64c5f8e4a77d677d366d478a61764011
Nestor <ncervan87@gmail.com>
2020-09-22 22:20:11 -0700

    no changes

--- trucks/trucks-model.js
+++ trucks/trucks-model.js
@@ -12,10 +12,12 @@ function findAllTrucks() {
 async function findById(id) {
     const res = await db('trucks as t')
         .join('items as i', 'i.truck_id', 't.id')
+        .leftJoin('users_items as ui', 'ui.item_id', 'i.id')
         .where('t.id', id)
-        .select('t.name as truck_name', 't.location', 't.id as truck_id', 'i.name', 'i.description', 'i.photo_url', 'i.price')
-
+        .select('t.name as truck_name', 't.location', 't.id as truck_id', 'i.name', 'i.id as food_id', 'i.description', 'i.photo_url', 'i.price', 'ui.ratings')
+    console.log(res)
     if (res.length > 0) {
+
         return {
             truck_name: res[0].truck_name,
             location: res[0].location,
@@ -23,9 +25,11 @@ async function findById(id) {
             food_items: res.map(item => (
                 {
                     name: item.name,
+                    food_id: item.food_id,
                     description: item.description,
                     photo_url: item.photo_url,
-                    price: item.price
+                    price: item.price,
+                    ratings: item.ratings
                 }
             ))
         }
